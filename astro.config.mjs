e04d0344bfca07ea9c511c53d2abf5cff4e027c2
Laurence <gkakese@gmail.com>
2026-03-22 19:07:54 +0200

    Fix build - add index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://adminbrighthorizon-hash.github.io',
-  base: '/alternative12-npo-website',
+  site: 'https://copakgt.github.io',
+  base: '/copakgt-website',
 });
